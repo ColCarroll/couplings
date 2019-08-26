94050d4b74a8ed06f7f1054c62578ff350d8f25a
Colin Carroll <colcarroll@gmail.com>
2019-08-26 17:22:43 -0400

    More setup nonsense

--- couplings/__init__.py
+++ couplings/__init__.py
@@ -1,3 +1,4 @@
 """Top level imports."""
+__version__ = "0.0.1"
 from .maximal_couplings import *
 from .metropolis_hastings import *
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,10 +2,10 @@
 
 set -ex # fail on first error, print commands
 
-SRC_DIR=${SRC_DIR:-`pwd`}
+SRC_DIR=${SRC_DIR:-$(pwd)}
 
 echo "Checking documentation..."
-python -m pydocstyle --convention=numpy ${SRC_DIR}/couplings/
+python -m pydocstyle --convention=numpy "${SRC_DIR}"/couplings/
 echo "Success!"
 
 echo "Checking code style with black..."
